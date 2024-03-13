4381ab5102473e79c510ffc1f06cec9e7b18d603
gurpej singh <68013567+gurpejsingh13@users.noreply.github.com>
2024-03-13 08:27:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='punjabi_stemmer',  # Package name
-    version='1.0.0',  # Initial version
-    packages=find_packages(),  # Automatically find and include all packages
+    name='punjabi_stemmer',
+    version='1.0.0',
+    packages=find_packages(),
     description='A Python library for stemming Punjabi language words, including preprocessing for noise removal.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Gurpej Singh',  # Your name
-    author_email='gurpejsingh462@gmail.com',  # Your email
-    url='https://github.com/gurpejsingh13/Punjabi_Stemmer.git',  # Replace with your repository URL
+    author='Gurpej Singh',
+    author_email='gurpejsingh462@gmail.com',
+    url='https://github.com/gurpejsingh13/Punjabi_Stemmer.git',
     license='MIT',
-    keywords=['stemmer', 'punjabi', 'nlp', 'punjabi language', 'natural language processing', 'text processing', 'noise removal'],
+    keywords=['stemmer', 'punjabi', 'Gurmukhi', 'nlp', 'punjabi language', 'natural language processing', 'text processing', 'noise removal',
+              'Stemming', 'Brute Force Algorithm', 'Suffix Striping', 'Under-stemming', 'Over-stemming', 'Stemming Algorithm',],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -27,10 +28,8 @@ setup(
         'Programming Language :: Python :: 3.10',
     ],
     python_requires='>=3.7',
-    include_package_data=True,  # To include data files from the 'data' directory in the package
-    # Dependencies: assuming no external libraries were used beyond Python's standard library
+    include_package_data=True,
     install_requires=[
-        # List any third-party libraries your package needs here.
-        # Example: 'requests>=2.25.1',
+        # No dependencies file required 
     ],
 )
